b3fd2991ebaaa03c6ce1fc090e97d0a88244d645
mahmud1995 <makhmudgo95@gmail.com>
2024-09-04 22:58:12 +0900

    BRR: Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+console.log("EXECUTED");
+
+import moment  from "moment";
+const currenTime = moment().format("YYY MM DD");
+console.log(currenTime);
+
+const person : string = "Jony";
+const count : number = 29;
