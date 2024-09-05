1c0d9a5fb45ec06b2fe74d715e388b03c72fadcb
mahmud1995 <makhmudgo95@gmail.com>
2024-09-05 23:50:36 +0900

    feat: express integrations

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import path from 'path';
+/*
+    1-ENTRENCE
+*/
+const app = express();
+console.log("__dirname:", __dirname);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+
+
+/*
+    2-SESSIONS
+*/
+
+/*
+    3-VIEWS
+*/
+app.set('view', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+/*
+    4-ROUTERS
+*/
+
+export default app; // CommonJS: module.exports = , 
--- src/server.ts
+++ src/server.ts
@@ -12,10 +12,16 @@ dotenv.config();
 // CLUSTER => DATABASE => COLLECTION => DOCUMENT
 
 import mongoose from 'mongoose';
+import app from "./app";
 
-mongoose.connect(process.env.MONGO_URL as string, {})
+
+mongoose
+    .connect(process.env.MONGO_URL as string, {})
     .then((data)=> {
-        console.log("MongoDB connection succes");
+        console.log("MongoDB connection succed");
         const PORT = process.env.PORT ?? 3003;
+        app.listen(PORT, function() {
+            console.log(`The server is running successfully on port: ${PORT}`);
+        })
     })
     .catch((err) => console.log("ERROR on connection MongDB", err));
