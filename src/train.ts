42ce2148f9a28a3060eb4342d83e3d7ed1f01802
mahmud1995 <makhmudgo95@gmail.com>
2024-09-06 00:28:24 +0900

    feat: H-TASK is DONE

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,35 @@
+
+/*
+H-TASK: 
+
+shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, 
+faqat positive qiymatlarni olib string holatda return qilsin
+MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+
+*/
+// function getPositive(arr: number[]): string {
+//   return arr.filter(num => num > 0).join('');
+// }
+// const result1 = getPositive([1,-4,2])
+// console.log();
+
+function getPositive(arr: number[]): string {
+  let result = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      result += arr[i];
+    }
+  }
+  return result;
+}
+
+console.log(getPositive([1,-4, 2]));
+
+
+
+
+
 /*G-TASK: 
 
 Shunday function tuzingki unga integerlardan iborat
@@ -8,22 +40,22 @@ MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 @MITASK
 */
 
-function getHighestIndex(arr: number[]) {
-    // defining new variable
-    let greatest;
-    let greatestIndex;
-    // looping through the arr parameter of function
-    for (let i = 0; i < arr.length; i++) {
-      // giving the condition if a found number is not the greatest
-      // in that case arr[i] is greater than that.
-      if (!greatest || arr[i] > greatest) {
-        greatest = arr[i];
-        // if not the greatestIndex = looped array of i
-        greatestIndex = i;
-      }
-    }
-    return greatestIndex;
-  }
-  let array = [5, 21, 12, 21, 8];
-  const result = getHighestIndex(array);
-  console.log("result:", result);
+// function getHighestIndex(arr: number[]) {
+//     // defining new variable
+//     let greatest;
+//     let greatestIndex;
+//     // looping through the arr parameter of function
+//     for (let i = 0; i < arr.length; i++) {
+//       // giving the condition if a found number is not the greatest
+//       // in that case arr[i] is greater than that.
+//       if (!greatest || arr[i] > greatest) {
+//         greatest = arr[i];
+//         // if not the greatestIndex = looped array of i
+//         greatestIndex = i;
+//       }
+//     }
+//     return greatestIndex;
+//   }
+//   let array = [5, 21, 12, 21, 8];
+//   const result = getHighestIndex(array);
+//   console.log("result:", result);
