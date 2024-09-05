90ee54c73c4d36cf9de7fbb3f145f516680ba7cf
mahmud1995 <makhmudgo95@gmail.com>
2024-09-05 23:29:58 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -7,3 +7,15 @@ dotenv.config();
 
 // console.log("MONGO_URL:",process.env.MONGO_URL);
 
+// // // // // // // // // / /
+
+// CLUSTER => DATABASE => COLLECTION => DOCUMENT
+
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_URL as string, {})
+    .then((data)=> {
+        console.log("MongoDB connection succes");
+        const PORT = process.env.PORT ?? 3003;
+    })
+    .catch((err) => console.log("ERROR on connection MongDB", err));
