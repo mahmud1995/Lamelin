b703b78bb887b0dbe180456d4bbc6d3820c20d7c
mahmud1995 <makhmudgo95@gmail.com>
2024-09-09 03:09:18 +0900

    feat: member service model integration

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,30 +1,8 @@
 import {Request, Response} from 'express';
 import {T} from "../libs/types/common";
 
+// REACT
+
 const memberController: T = {};
-
-memberController.goHome = (req: Request, res: Response) => {
-    try {
-        res.send("Home Page")
-
-    } catch(err) {
-        console.log("ERROR, goHome:", err)
-    }
-    res.send('Home Page');
-};
-memberController.getLogin = (req: Request, res: Response) => {
-    try {
-        res.send('Login Page');
-    } catch(err) {
-        console.log("ERROR, getLogin:", err)
-    }
-};
-memberController.getSignup = (req: Request, res: Response) => {
-    try {
-        res.send('Sign-up PAge');
-    } catch(err) {
-        console.log("ERROR, getSignup:", err)
-    }
-};
 
 export default memberController;
