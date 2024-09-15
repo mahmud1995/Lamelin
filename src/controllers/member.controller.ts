a2e62479dff79c4dd005e99aa366f7929c771218
mahmud1995 <makhmudgo95@gmail.com>
2024-09-15 13:55:38 +0900

    fix: Organise SPA and SSR business logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,37 +1,41 @@
 import {Request, Response} from 'express';
-import {T} from "../libs/types/common";
-import MemberService from '../models/Member.service';
+import { T } from "../libs/types/common";
 import { LoginInput, Member, MemberInput } from '../libs/types/member';
-import { MemberType } from '../libs/enums/member.enum';
-
-// REACT
+import MemberService from '../models/Member.service';
+import Errors from '../libs/Errors';
+// import { MemberType } from '../libs/enums/member.enum';
+//                      REACT
 const memberService = new MemberService();
-
 const memberController: T = {};
+
 memberController.signup = async (req: Request, res:Response) => {
     try {
         console.log("signup");
+        console.log("body:", req.body); // // // Fixed
         const input: MemberInput = req.body,
             result: Member = await memberService.signup(input);
+        // TODO: TOKEN
 
-        // res.json({ member: result });
-        res.send("DONE!!!!!");
+        res.json({ member: result });    // // // Fixed
+        // res.send("DONE!!!!!");
     } catch (err) {
-        console.log("Error, signup", err);
-        // res.json({ });
+        console.log("Error, signup:", err);
+        res.status(Errors.standard.code).json(Errors.standard);
     }
 };
 
 memberController.login = async (req: Request, res: Response) => {
     try {
         console.log("login");
+        console.log("body:", req.body);  // // // Fixed
         const input: LoginInput = req.body,
             result = await memberService.login(input);
+        // TODO: TOKEN
 
         res.json({ member: result });
     } catch (err) {
         console.log("Error, processLogin:", err);
-        // res.send({ });
+        res.status(Errors.standard.code).json(Errors.standard);
     }
 };
 
--- src/server.ts
+++ src/server.ts
@@ -14,10 +14,10 @@ import app from "./app";
 mongoose
     .connect(process.env.MONGO_URL as string, {})
     .then((data)=> {
-        console.log("MongoDB connection succed");
+        console.log("MongoDB connection successed!");
         const PORT = process.env.PORT ?? 3003;
         app.listen(PORT, function() {
-            console.log(`The server is running successfully on port: ${PORT},http://localhost:${PORT}`)
+            console.log(`The server is running successfully on port: ${PORT}`)
         })
     })
     .catch((err) => console.log("ERROR on connection MongDB", err));
