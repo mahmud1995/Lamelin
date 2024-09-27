49fa9be86be6baac648ddf4313a8c7f99b5d4098
mahmud1995 <makhmudgo95@gmail.com>
2024-09-27 17:08:21 +0900

    feat: develop updateChosenUser api

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,4 +1,5 @@
 export const MORGAN_FORMAT = `:method :url :response-time [:status] \n`;
+
 import mongoose from "mongoose";
 export const shapeIntoMongooseObectId = (target: any) => {
     return typeof target === 'string' ? new mongoose.Types.ObjectId(target) : target;
