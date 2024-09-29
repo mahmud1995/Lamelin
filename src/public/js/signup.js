50c954882014220afc13a9f053f0e3a51b24c41b
mahmud1995 <makhmudgo95@gmail.com>
2024-09-30 01:57:43 +0900

    feat: Admin frontendvalidation 54-dars

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,5 +1,31 @@
 console.log("Signup frontend javascript file");
 
+$(function () {
+    const fileTarget = $(".file-box .upload-hidden");
+    let filename;
+
+    fileTarget.on("change", function () {
+        if(window.FileReader) {
+            const uploadFile = $(this)[0].files[0];
+            const fileType = uploadFile["type"];
+            const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+            if(!validImageType.includes(fileType)) {
+                alert("Please insert only jpeg, jpg, and png!");
+            } else {
+                if(uploadFile) {
+                    console.log(URL.createObjectURL(uploadFile));
+                    $(".upload-img-frame")
+                        .attr("src", URL.createObjectURL(uploadFile))
+                        .addClass("succes");
+                }
+                filename = $(this)[0].files[0].name;
+            }
+            $(this).siblings(".upload-name").val(filename);
+        };
+    });
+})
+
+
 function validateSignupForm() {
     const memberNick = $(".member-nick").val();
     const memberPhone = $(".member-phone").val();
@@ -22,4 +48,12 @@ function validateSignupForm() {
         alert("Passwords are different, Please check!");
         return false;
     }
+    
+    const memberImage = $(".member-image").get(0).files[0] 
+        ? $("member-image").get(0).files[0].name
+        : null;
+        if(!memberImage) {
+            alert("Please insert restaurant image!");
+            return false; // backend ga request qilmasligi ucun
+        }
 };
